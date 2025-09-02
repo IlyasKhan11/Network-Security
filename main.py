c5d8641ae342b1ffa52a21f72a89c2e8c6d8554c
IlyasKhan11 <ilyas.dev2021@gmail.com>
2025-09-02 21:32:47 +0500

    model trainer and selection

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 
 
 import sys
@@ -15,7 +16,7 @@ if __name__ == '__main__':
         dataIngestionConfig=DataIngestionConfig(trainingPipelineConfig)
         data_validation_config=DataValidationConfig(trainingPipelineConfig)
         data_transformation_config=DataTransformationConfig(trainingPipelineConfig)
-        
+        model_trainer_config=ModelTrainerConfig(trainingPipelineConfig)
         
         
         data_ingestion=DataIngestion(dataIngestionConfig)
@@ -36,6 +37,12 @@ if __name__ == '__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation completed")
+
+        logging.info("Model Trainer started")
+        model_trainer_config=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer_config.initiate_model_trainer()
+        logging.info("Model Trainer Done")
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
